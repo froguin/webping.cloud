489037bfb4d19241c4464b4799862217aaca49c9
SeongHan Kim <froguin@users.noreply.github.com>
2026-05-04 22:56:34 +0900

    feat: add all missing country codes (AT, CO, DK, MA, NZ, QA, RS)

--- src/fns/country.ts
+++ src/fns/country.ts
@@ -1,5 +1,6 @@
 const codeToName: { [code: string]: string } = {
   AE: 'United Arab Emirates',
+  AT: 'Austria',
   AU: 'Australia',
   BE: 'Belgium',
   BH: 'Bahrain',
@@ -8,7 +9,9 @@ const codeToName: { [code: string]: string } = {
   CH: 'Switzerland',
   CL: 'Chile',
   CN: 'China',
+  CO: 'Colombia',
   DE: 'Germany',
+  DK: 'Denmark',
   ES: 'Spain',
   FI: 'Finland',
   FR: 'France',
@@ -20,12 +23,16 @@ const codeToName: { [code: string]: string } = {
   IT: 'Italy',
   JP: 'Japan',
   KR: 'South Korea',
+  MA: 'Morocco',
   MX: 'Mexico',
   MY: 'Malaysia',
   NL: 'Netherlands',
   NO: 'Norway',
+  NZ: 'New Zealand',
   PH: 'Philippines',
   PL: 'Poland',
+  QA: 'Qatar',
+  RS: 'Serbia',
   SA: 'Saudi Arabia',
   SE: 'Sweden',
   SG: 'Singapore',
@@ -39,7 +46,7 @@ const codeToName: { [code: string]: string } = {
 export function getCountryName(countryCode: string): string {
   const name = codeToName[countryCode.toUpperCase()]
   if (!name) {
-    throw new Error(`Country name not found for code '${countryCode}')`)
+    throw new Error(`Country name not found for code '${countryCode}'`)
   }
   return name
 }
