ab7a2d0fb2a74399361062672f90f11cbe3951fa
SeongHan Kim <froguin3@gmail.com>
2026-05-18 00:09:36 +0900

    feat: integrate Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from 'next/app'
 import React from 'react'
+import { Analytics } from '@vercel/analytics/react'
 import './globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
