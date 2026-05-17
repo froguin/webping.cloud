d10c00489dd0bb99a9c599cf9d8f480079640aff
SeongHan Kim <froguin3@gmail.com>
2026-05-17 21:06:20 +0900

    fix: ensure consistent logo sizing and aspect ratio

--- src/components/country-flag.tsx
+++ src/components/country-flag.tsx
@@ -10,13 +10,29 @@ export interface CountryFlagProps {
 export function CountryFlag(props: CountryFlagProps): JSX.Element {
   const countryName = getCountryName(props.countryCode)
   return (
-    <Image
-      width={props.width}
-      height={props.width}
-      className={`inline ${props.className}`}
-      src={`/images/country/${props.countryCode.toLowerCase()}.svg`}
-      title={countryName}
-      alt={countryName}
-    />
+    <div
+      style={{
+        width: props.width,
+        height: props.width,
+        display: 'inline-flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        verticalAlign: 'middle',
+      }}
+    >
+      <Image
+        width={props.width}
+        height={props.width}
+        style={{
+          objectFit: 'contain',
+          width: '100%',
+          height: '100%',
+        }}
+        className={`inline ${props.className}`}
+        src={`/images/country/${props.countryCode.toLowerCase()}.svg`}
+        title={countryName}
+        alt={countryName}
+      />
+    </div>
   )
 }
--- src/components/provider-logo.tsx
+++ src/components/provider-logo.tsx
@@ -9,13 +9,28 @@ export interface CloudProviderLogoProps {
 
 export function CloudProviderLogo(props: CloudProviderLogoProps): JSX.Element {
   return (
-    <Image
-      width={props.width}
-      height={props.width}
-      className={`inline ${props.className}`}
-      src={`/images/provider/${props.providerKey}.svg`}
-      title={props.providerName}
-      alt={props.providerName}
-    />
+    <div
+      style={{
+        width: props.width,
+        height: props.width,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Image
+        width={props.width}
+        height={props.width}
+        style={{
+          objectFit: 'contain',
+          width: '100%',
+          height: '100%',
+        }}
+        className={`inline ${props.className}`}
+        src={`/images/provider/${props.providerKey}.svg`}
+        title={props.providerName}
+        alt={props.providerName}
+      />
+    </div>
   )
 }
